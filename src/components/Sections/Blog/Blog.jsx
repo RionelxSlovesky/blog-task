73422108414bab0053d5bad02f34434fb056926c
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-17 16:05:00 +0600

    Created tabs for blogs
    Created the tab structure, need to update the contents

--- src/components/Sections/Blog/Blog.jsx
+++ src/components/Sections/Blog/Blog.jsx
@@ -1,12 +1,47 @@
 import SectionTitle from "../../SectionTitle/SectionTitle";
-
+import { TabPanel, useTabs } from "react-headless-tabs";
+import TabSelector from "./TabSelector/TabSelector";
 
 const Blog = () => {
-    return (
-        <div>
-            <SectionTitle title='Blog'></SectionTitle>
+  const [selectedTab, setSelectedTab] = useTabs([
+    "sports",
+    "entertainment",
+    "health",
+    "billing",
+  ]);
+
+  return (
+    <section>
+      <SectionTitle title="Blog"></SectionTitle>
+      <div>
+        <nav className="flex justify-center">
+          <TabSelector
+            isActive={selectedTab === "sports"}
+            onClick={() => setSelectedTab("sports")}
+          >
+            Sports
+          </TabSelector>
+          <TabSelector
+            isActive={selectedTab === "entertainment"}
+            onClick={() => setSelectedTab("entertainment")}
+          >
+            Entertainment
+          </TabSelector>
+          <TabSelector
+            isActive={selectedTab === "health"}
+            onClick={() => setSelectedTab("health")}
+          >
+            Health
+          </TabSelector>
+        </nav>
+        <div className="p-4">
+          <TabPanel hidden={selectedTab !== "sports"}>Sports</TabPanel>
+          <TabPanel hidden={selectedTab !== "entertainment"}>Entertainment</TabPanel>
+          <TabPanel hidden={selectedTab !== "health"}>Health</TabPanel>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
---
+++ src/components/Sections/Blog/TabSelector/TabSelector.jsx
@@ -0,0 +1,16 @@
+const TabSelector = ({ isActive, children, onClick }) => {
+  return (
+    <button
+      className={`mr-8 group inline-flex items-center px-2 py-4 border-b-2 font-medium text-lg leading-5 cursor-pointer whitespace-nowrap ${
+        isActive
+          ? "border-red-500 text-red-600 focus:outline-none focus:text-red-800 focus:border-red-700"
+          : "border-transparent text-gray-500 hover:text-gray-600 hover:border-red-300 focus:text-gray-600 focus:border-red-300"
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default TabSelector;
