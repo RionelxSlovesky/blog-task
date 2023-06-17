c4a6785849947ca29ccec7ebf204e37497bd323a
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-17 14:42:15 +0600

    Added Marquee to the Navbar
    Added react marque for displaying latest news/tips. Will change the contents later.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Navbar from './components/Navbar/Navbar'
 function App() {
 
   return (
-    <>
+    <div className='max-w-[1400px] mx-auto'>
       <Navbar></Navbar>
-    </>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,59 @@
 import { Link } from "react-router-dom";
-import {FaBars, FaTimes} from 'react-icons/fa'
+import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
-import './Navbar.css'
-
+import "./Navbar.css";
+import Marquee from "react-fast-marquee";
 
 const Navbar = () => {
+  const navRef = useRef();
 
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    const navRef = useRef()
-
-    const showNavbar = () => {
-        navRef.current.classList.toggle('responsive_nav')
-    }
-
-    return (
-        <header className="flex items-center justify-center py-6 bg-cyan-500 text-white px-[2rem] text-xl">
-            <Link className="flex-1" to='/'><h3>Blog</h3></Link>
-            <nav
-            ref={navRef}>
-                <Link className="mx-[2em] hover:text-yellow-500" to='/'>About</Link>
-                <Link className="mx-[2em] hover:text-yellow-500" to='/'>Contact</Link>
-                <Link className="mx-[2em] hover:text-yellow-500" to='/'>Login</Link>
-                <button 
-                className="p-[5px] cursor-pointer bg-transparent
-                md:hidden md:opacity-0" 
-                onClick={showNavbar}>
-                    <FaTimes></FaTimes>
-                </button>
-            </nav>
-            <button 
+  return (
+    <>
+      <header className="flex items-center justify-center py-6 bg-cyan-500 text-white px-[2rem] text-xl">
+          <Link to="/" className="me-auto">
+            <h3>Blog</h3>
+          </Link>
+        <nav ref={navRef}>
+          <Link className="mx-[2em] hover:text-yellow-500" to="/">
+            About
+          </Link>
+          <Link className="mx-[2em] hover:text-yellow-500" to="/">
+            Contact
+          </Link>
+          <Link className="mx-[2em] hover:text-yellow-500" to="/">
+            Login
+          </Link>
+          <button
             className="p-[5px] cursor-pointer bg-transparent
-            md:hidden md:opacity-0" 
-            onClick={showNavbar}>
-                <FaBars></FaBars>
-            </button>
-        </header>
-    );
+                md:hidden md:opacity-0"
+            onClick={showNavbar}
+          >
+            <FaTimes></FaTimes>
+          </button>
+        </nav>
+        <button
+          className="p-[5px] cursor-pointer bg-transparent
+            md:hidden md:opacity-0"
+          onClick={showNavbar}
+        >
+          <FaBars></FaBars>
+        </button>
+      </header>
+      <div className="flex text-lg">
+        <div className="flex-grow py-2 px-4 bg-yellow-600 text-white">
+          Breaking:
+        </div>
+        <Marquee>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
