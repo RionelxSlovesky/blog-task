9f8837bc40f9d4388e3e98cac0541d9cc1101494
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-18 14:58:37 +0600

    Responsive Issue
    Decreased the margin between tab selector buttons since it was causing responsive issue in small devices

--- src/components/Sections/Blog/TabSelector/TabSelector.jsx
+++ src/components/Sections/Blog/TabSelector/TabSelector.jsx
@@ -1,7 +1,7 @@
 const TabSelector = ({ isActive, children, onClick }) => {
   return (
     <button
-      className={`mr-8 group inline-flex items-center px-2 py-4 border-b-2 font-medium text-lg leading-5 cursor-pointer whitespace-nowrap ${
+      className={`mr-4 group inline-flex items-center px-2 py-4 border-b-2 font-medium text-lg leading-5 cursor-pointer whitespace-nowrap ${
         isActive
           ? "border-red-500 text-red-600 focus:outline-none focus:text-red-800 focus:border-red-700"
           : "border-transparent text-gray-500 hover:text-gray-600 hover:border-red-300 focus:text-gray-600 focus:border-red-300"
