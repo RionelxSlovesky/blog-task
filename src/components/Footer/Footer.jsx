595c813c87edd830913162d7d073841f2211dfbb
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-18 11:24:26 +0600

    Created Footer
    Created a simple footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white text-center py-10 md:py-16">
+            <h2 className="pb-[0.3em] text-2xl md:text-4xl">Blog</h2>
+            <h4 className="pb-[1.5em] text-lg md:text-xl">Copyright © 2023 Blog, Inc.</h4>
+            <p className="text-sm md:text-lg"><Link>About</Link> | <Link>Contact</Link> | <Link>Policy</Link></p>
+        </footer>
+    );
+};
+
+export default Footer;
